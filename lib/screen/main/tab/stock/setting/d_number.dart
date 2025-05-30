0360abc2a9c55b5b375b9efaf694a7051261a65b
yeseo <ye_2002@naver.com>
2025-05-30 14:56:34 +0900

    설정 화면

---
+++ lib/screen/main/tab/stock/setting/d_number.dart
@@ -0,0 +1,41 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/common/widget/w_round_button.dart';
+import 'package:fast_app_base/common/widget/w_rounded_container.dart';
+import 'package:flutter/material.dart';
+import 'package:nav/dialog/dialog.dart';
+
+class NumberDialog extends DialogWidget<int?> {
+  NumberDialog({super.key, super.animation = NavAni.Fade, super.barrierDismissible = false});
+
+  @override
+  DialogState<NumberDialog> createState() => _NotificationDialogState();
+}
+
+class _NotificationDialogState extends DialogState<NumberDialog> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RoundedContainer(
+              child: Column(
+                children: [
+                  '숫자를 입력해주세요'.text.make(),
+                  TextField(controller: controller, keyboardType: TextInputType.number,),
+                  height10,
+                  RoundButton(text: '완료', onTap: (){
+                    final text = controller.text;
+                    int number = int.parse(text);
+                    widget.hide(number);
+                  })
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
